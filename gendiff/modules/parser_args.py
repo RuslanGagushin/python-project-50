bf6bd73ebfbd4a6192915c2eae81df0efce4da45
Ruslan <gagushin2@gmail.com>
2024-05-09 17:43:24 +0400

    2 stage done

--- gendiff/modules/parser_args.py
+++ gendiff/modules/parser_args.py
@@ -5,7 +5,14 @@ def parser_args():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument("first_file", help="first file")
+    parser.add_argument("second_file", help="second file")
+    parser.add_argument(
+        "-f",
+        "--format",
+        help="set format of output",
+        default="stylish",
+        choices=["stylish", "plain", "json"],
+    )
     args = parser.parse_args()
     return args
